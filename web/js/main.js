6903293e7aade2f0e3a1faa3d5a09f236eea3659
Otto Colomina Pardo <otto@dccia.ua.es>
2016-11-23 17:54:46 +0100

    versión inicial que puede usarse como plantilla de partida

---
+++ web/js/main.js
@@ -0,0 +1,5 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+
+var MiComponente = require('./MiComponente')
+ReactDOM.render(<MiComponente/>, document.getElementById('miComponente'))
